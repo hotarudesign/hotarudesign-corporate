ed580ff222902675f6bfcd59f71f0d48f80b3e8f
hotarudesign <hotarudesign1220@gmail.com>
2023-09-02 14:32:30 +0900

    💄 headerの作成

--- src/header.php
+++ src/header.php
@@ -4,9 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test_wp</title>
+    <title>Hotaru Design</title>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <header class="header"></header>
+    <header class="header">
+        <div class="header__inner">
+            <h1 class="header__logo logo">
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                    <img src='<?php echo ImgPath; ?>/logo-black.svg' alt='Hotaru Design'>
+                </a>
+            </h1>
+            <nav class="g-nav">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'g-nav',
+                        'container' => '',
+                        'menu_class' => 'g-nav__list',
+                    )
+                );
+                ?>
+            </nav>
+        </div>
+    </header>
