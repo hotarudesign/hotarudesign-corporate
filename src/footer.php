c809030e7ba305c6f97d47de2c6f0eae11ff54d7
hotarudesign <hotarudesign1220@gmail.com>
2023-09-07 06:33:07 +0900

    ✨ footerの追加

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,20 @@
-<footer class="footer"></footer>
+<footer class="footer">
+    <div class="footer__inner inner">
+        <a href="<?php echo esc_url(home_url('/')); ?>" class="footer__logo">
+            <img src="<?php echo ImgPath; ?>/logo-white-nobg.svg" alt="">
+        </a>
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'f-nav',
+                'container' => '',
+                'menu_class' => 'f-nav__list',
+            )
+        );
+        ?>
+    </div>
+    <p class="footer__copy"><small>&copy; Hotaru Design</small></p>
+</footer>
 <?php wp_footer(); ?>
 </body>
 
