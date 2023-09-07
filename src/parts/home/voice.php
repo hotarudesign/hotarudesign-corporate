0b87934490f0d2d79d33f3aec898d2e9df53a281
hotarudesign <hotarudesign1220@gmail.com>
2023-09-08 06:43:35 +0900

    ✨ voiceセクションの追加

--- src/parts/home/voice.php
+++ src/parts/home/voice.php
@@ -1,18 +1,36 @@
 <section class="voice">
     <div class="voice__inner inner">
         <h2 class="voice-ttl sec-ttl">Voice</h2>
-        <div class="splide">
-            <div class="splide__track">
-                <ol class="splide__list voice__list">
-                    <li class="splide__slide voice__item">
-                        <div class="voice__item__txt">
-                            <h3 class="voice__item__txt-ttl"></h3>
-                            <p class="voice__item__txt-desc"></p>
-                        </div>
-                        <figure class="voice__item-thumb"></figure>
-                    </li>
-                </ol>
-            </div>
-        </div>
+        <ol class="voice__list">
+            <li class="voice__item">
+                <div class="voice__item__txt">
+                    <h3 class="voice__item__txt-ttl">株式会社AAA</h3>
+                    <p class="voice__item__txt-desc">いつも素早い対応に感謝しています。制作してくれたコーポレートサイトやLPのデザインも私の拘りを抑えた上で制作してくれました。
+                    </p>
+                </div>
+                <figure class="voice__item-avatar">
+                    <img src='<?php echo ImgPath; ?>/voice_item01.svg' alt=''>
+                </figure>
+            </li>
+            <li class="voice__item">
+                <div class="voice__item__txt">
+                    <h3 class="voice__item__txt-ttl">株式会社BBB</h3>
+                    <p class="voice__item__txt-desc">凝ったアニメーションが必要でしたが、希望通りのWebサイトを制作していただくことができました。素早く丁寧な対応には驚きました。
+                    </p>
+                </div>
+                <figure class="voice__item-avatar">
+                    <img src='<?php echo ImgPath; ?>/voice_item02.svg' alt=''>
+                </figure>
+            </li>
+            <li class="voice__item">
+                <div class="voice__item__txt">
+                    <h3 class="voice__item__txt-ttl">株式会社CCC</h3>
+                    <p class="voice__item__txt-desc">いつも丁寧なコミュニケーションをとってくれて、こちらが気が付かないような欠点も事前に気がつかせてくれました。</p>
+                </div>
+                <figure class="voice__item-avatar">
+                    <img src='<?php echo ImgPath; ?>/voice_item03.svg' alt=''>
+                </figure>
+            </li>
+        </ol>
     </div>
 </section>
