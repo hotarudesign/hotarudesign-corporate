4ea6aa30bb7d707abaf5d8bdea9088243f58ec4f
hotarudesign <hotarudesign1220@gmail.com>
2023-09-07 17:22:35 +0900

    ✨ ctaの追加

--- src/parts/home/voice.php
+++ src/parts/home/voice.php
@@ -1,5 +1,18 @@
 <section class="voice">
     <div class="voice__inner inner">
         <h2 class="voice-ttl sec-ttl">Voice</h2>
+        <div class="splide">
+            <div class="splide__track">
+                <ol class="splide__list voice__list">
+                    <li class="splide__slide voice__item">
+                        <div class="voice__item__txt">
+                            <h3 class="voice__item__txt-ttl"></h3>
+                            <p class="voice__item__txt-desc"></p>
+                        </div>
+                        <figure class="voice__item-thumb"></figure>
+                    </li>
+                </ol>
+            </div>
+        </div>
     </div>
 </section>
